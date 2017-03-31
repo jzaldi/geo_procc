e6d235fd7fd9bbf6204c5b9628ef842743077e51
zaldi <jzaldi89@gmail.com>
2017-03-31 18:15:57 +0200

    added build folder

---
+++ CMakeFiles/geo_procc.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zaldi/Desktop/geo_procc/cell.cpp" "/home/zaldi/Desktop/geo_procc/CMakeFiles/geo_procc.dir/cell.cpp.o"
+  "/home/zaldi/Desktop/geo_procc/gdal_raster.cpp" "/home/zaldi/Desktop/geo_procc/CMakeFiles/geo_procc.dir/gdal_raster.cpp.o"
+  "/home/zaldi/Desktop/geo_procc/main.cpp" "/home/zaldi/Desktop/geo_procc/CMakeFiles/geo_procc.dir/main.cpp.o"
+  "/home/zaldi/Desktop/geo_procc/raster.cpp" "/home/zaldi/Desktop/geo_procc/CMakeFiles/geo_procc.dir/raster.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/geo_procc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geo_procc.dir/main.cpp.o"
+  "CMakeFiles/geo_procc.dir/cell.cpp.o"
+  "CMakeFiles/geo_procc.dir/raster.cpp.o"
+  "CMakeFiles/geo_procc.dir/gdal_raster.cpp.o"
+  "geo_procc.pdb"
+  "geo_procc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geo_procc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
